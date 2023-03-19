52aeaa1d1219f0d22622aedc7dc2e90ac6cda48b
Erione White <erionewhite1999@gmail.com>
2023-03-18 20:33:23 -0500

    initialized package.json, connected to mongoose, connected to server

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://localhost/foods', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', err => console.error(err))
+db.once('open', () => console.log('Connected to database'))
+
+
+export default mongoose
