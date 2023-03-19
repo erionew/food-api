76dec657e05a0dffacae11430d64ef2e4b98e537
Erione White <erionewhite1999@gmail.com>
2023-03-18 22:22:41 -0500

    /foods/ will return all foods

--- routes/foods.js
+++ routes/foods.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import Food from '../models/Food.js'
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Food.find({})
+        .then(food => {
+            res.json(food)
+        })
+})
 
 export default router
